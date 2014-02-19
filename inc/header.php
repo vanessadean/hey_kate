03cf3a362f3b761373286b783a5bff9df1ce9580
Vanessa Dean <vanessa.dean@gmail.com>
2014-02-18 22:24:10 -0500

    Style update

--- inc/header.php
+++ inc/header.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <!--[if IE 8]>         <html class="no-js lt-ie9" lang="en" > <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js" lang="en" > <!--<![endif]-->
-
 <html class="no-js" lang="en">
+<script src="https://raw.github.com/wilto/iOS-orientationchange-fix.js"></script>
+
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -17,10 +18,10 @@
   <div class="header">
     <div class="row">
       <div class="large-6 medium-6 small-12 columns header-1">
-        <h1 class="reviewone">Hey, Kate!</h1>
+        <h1 class="left-bubble">Hey, Kate!</h1>
       </div>
       <div class="large-6 medium-6 columns hide-for-small header-2">
-        <h1 class="reviewtwo">Whaddya want?!</h1>
+        <h1 class="right-bubble">Whaddya want?!</h1>
       </div>
     </div>
   </div>
--- index.php
+++ index.php
@@ -7,17 +7,17 @@ include('inc/content.php');
 
   <div class="row">
       <div class="large-12 small-12 columns">
-        <h3 class="reviewone">Would you pretty please make me<br><a href=<?php echo $links[$recipe]; ?> target="_blank"> <?php echo $recipes[$recipe]; ?></a>?</h3>
+        <h3 class="left-bubble">Would you pretty please make me<br><a href=<?php echo $links[$recipe]; ?> target="_blank"> <?php echo $recipes[$recipe]; ?></a>?</h3>
       </div>
   </div>
   <div class="row">
       <div class="large-12 small-12 columns">
-        <h3 class="reviewtwo"><?php echo $retorts[$retort]; ?></h3>
+        <h3 class="right-bubble"><?php echo $retorts[$retort]; ?></h3>
       </div>
   </div>
   <div class="row">
       <div class="large-12 small-12 columns">
-        <h3 class="reviewone">Okay, <a href="./">how about...</a></h3>
+        <h3 class="left-bubble">Okay, <a href="./">how about...</a></h3>
       </div>
   </div>
 
